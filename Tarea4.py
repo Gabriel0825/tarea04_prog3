8d0fd5f2d03f668d3f2f40a05d87c60c88386883
Gabriel0825 <62782892+Gabriel0825@users.noreply.github.com>
2024-04-10 22:43:22 -0400

    Add files via upload

--- Tarea4.py
+++ Tarea4.py
@@ -30,10 +30,22 @@ LogIn= driver.find_element(By.XPATH, "//button[@type='button'][contains(.,'Log i
 
 # Historia de Usuario 3- Añadir Producto a carrito
 
-addToCart= driver.find_element(By.XPATH, "//a[@href='prod.html?idp_=4'][contains(.,'Samsung galaxy s7')]").click()
+"""addToCart= driver.find_element(By.XPATH, "//a[@href='prod.html?idp_=4'][contains(.,'Samsung galaxy s7')]").click()
 time.sleep(1)
+btnCart= driver.find_element(By.XPATH, "//a[@href='#'][contains(.,'Add to cart')]").click()
+Cart= driver.find_element(By.XPATH, "//a[@href='cart.html']").click()
+time.sleep(2)"""
 
 
+#Historia de Usuario 4- Enviar Mensaje a la tienda
+
+sendMessage= driver.find_element(By.XPATH, "//a[@class='nav-link'][contains(.,'Contact')]").click()
+time.sleep(1)
+contactMail= driver.find_element(By.XPATH, "//input[contains(@id,'recipient-email')]").send_keys("20210055@itla.com")
+contactName= driver.find_element(By.XPATH, "//input[contains(@id,'recipient-name')]").send_keys("Jeremy Garcia")
+Message= driver.find_element(By.XPATH, "//textarea[contains(@id,'message-text')]").send_keys("Hola Itla!!!")
+btn_send= driver.find_element(By.XPATH, "//button[@type='button'][contains(.,'Send message')]").click()
+time.sleep(2)
 #Tiempo de suspension de la pantalla (Segundos)
 time.sleep(20)
 
